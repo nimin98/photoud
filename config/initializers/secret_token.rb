4785749d32687e5d953493a288dd0881ba434362
Min Ni <nimin98@gmail.com>
2012-04-26 22:53:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoud::Application.config.secret_token = '6225bcda17a91d19dc35821418e0890f71faecd6b678e33f6890ddce1950d768704428fdc943a4ead40b4b4f4796469df874a992f6f423272278079fa641fbc5'
