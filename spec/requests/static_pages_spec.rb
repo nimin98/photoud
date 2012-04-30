ba0efb72ac0f2d6ae234f200f5858e21e84db6cc
Min Ni <nimin98@gmail.com>
2012-04-30 08:33:30 -0700

    setup guard for rspce; add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,24 +2,31 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Photoud" }
+
   describe "Home Page" do
-    it "should have the content 'Photoud'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Photoud | Home')
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do 
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help Page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Photoud | Help')
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => 'Photoud | About Us')
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
